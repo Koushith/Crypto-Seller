c75242a6e8788bdf8d05d92ffa1720f78c8ca202
koushith <koushith97@gmail.com>
2020-07-09 19:49:17 +0530

    ERC20

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
+const Token = artifacts.require('Token');
+
 const EthSwap = artifacts.require('EthSwap');
 
 module.exports = function(deployer) {
+  //  Deploy token
+  deployer.deploy(Token);
+  //  Deploy Ethswap
   deployer.deploy(EthSwap);
 };
