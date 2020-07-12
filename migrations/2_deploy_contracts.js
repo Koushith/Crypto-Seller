2210295c01628b5f15df2d47d67c47f3745fa4d8
koushith <koushith97@gmail.com>
2020-07-12 17:20:20 +0530

    solidity

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,6 @@
+// Goal here is to build the crypto exchage. which is 1 million by default. we are creating our own token and selling them at fixed price
+
+
 const Token = artifacts.require('Token');
 
 const EthSwap = artifacts.require('EthSwap');
@@ -5,6 +8,11 @@ const EthSwap = artifacts.require('EthSwap');
 module.exports = function(deployer) {
   //  Deploy token
   deployer.deploy(Token);
+  const token = await Token.deployed()
   //  Deploy Ethswap
   deployer.deploy(EthSwap);
+  const ethswap = await EthSwap.deployed()
+
+  // Transfer all tokens to EthSwap(1 mil)
+  await token.transfer(ethswap.adress, '1000000000000000000000000')
 };
