d00915db87119f8918881cab061f49bdf533651b
koushith <koushith97@gmail.com>
2020-07-14 14:13:50 +0530

    test cases

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,17 @@
 // Goal here is to build the crypto exchage. which is 1 million by default. we are creating our own token and selling them at fixed price
 
-
 const Token = artifacts.require('Token');
-
 const EthSwap = artifacts.require('EthSwap');
 
-module.exports = function(deployer) {
-  //  Deploy token
-  deployer.deploy(Token);
-  const token = await Token.deployed()
-  //  Deploy Ethswap
-  deployer.deploy(EthSwap);
-  const ethswap = await EthSwap.deployed()
+module.exports = async function(deployer) {
+  // Deploy Token
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
 
-  // Transfer all tokens to EthSwap(1 mil)
-  await token.transfer(ethswap.adress, '1000000000000000000000000')
+  // Deploy EthSwap
+  await deployer.deploy(EthSwap, token.address);
+  const ethSwap = await EthSwap.deployed();
+
+  // Transfer all tokens to EthSwap (1 million)
+  await token.transfer(ethSwap.address, '1000000000000000000000000');
 };
