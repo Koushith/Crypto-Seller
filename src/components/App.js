bb663807689a73c0365a7dd9b3ffa4af8cbeb86e
koushith <koushith97@gmail.com>
2020-07-14 23:45:43 +0530

    client

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3';
 import './App.css';
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3();
+
+    await this.loadBlockChainData();
+  }
+  // Metemask connection
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert('Non Etherium browser detectef');
+    }
+  }
+
+  async loadBlockChainData() {
+    const web3 = window.web3;
+    // from web3.js docs
+    const accounts = await web3.eth.getAccounts();
+
+    console.log(accounts);
+  }
+
   render() {
     return (
       <div>
@@ -13,31 +38,13 @@ class App extends Component {
             target='_blank'
             rel='noopener noreferrer'
           >
-            Dapp University
+            Crypto Trade
           </a>
         </nav>
         <div className='container-fluid mt-5'>
           <div className='row'>
             <main role='main' className='col-lg-12 d-flex text-center'>
-              <div className='content mr-auto ml-auto'>
-                <a
-                  href='http://www.dappuniversity.com/bootcamp'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img src={logo} className='App-logo' alt='logo' />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to relod.
-                </p>
-                <a
-                  className='App-link'
-                  href='http://www.dappuniversity.com/bootcamp'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                ></a>
-              </div>
+              <div className='content mr-auto ml-auto'>Hello World</div>
             </main>
           </div>
         </div>
